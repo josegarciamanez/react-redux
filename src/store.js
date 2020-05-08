3f6c01bcc17b151db99d3de468800ae5029b35c3
José García Máñez <josegarciamanez@gmail.com>
2020-05-08 18:48:38 +0200

    add store.js

---
+++ src/store.js
@@ -0,0 +1,54 @@
+import { createStore } from 'redux';
+
+// constantes de acciones
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+// Reducer
+const initialState = 0;
+
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+
+    case DECREMENT:
+      return state - 1;
+
+    default:
+      return state;
+  }
+}
+
+// Store
+const store = createStore(counter);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// Creadores de acciones
+const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+// Acciones
+store.dispatch(increment());
+
+setTimeout(() => {
+  store.dispatch(decrement());
+}, 2000);
+
+// store.getState()
+// store.dispatch()
+// store.subscribe(fun)
+
+export default store;
